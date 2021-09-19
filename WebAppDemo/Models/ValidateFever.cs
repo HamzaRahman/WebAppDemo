786d61ecbad2f55951fadbe114318bf62daac2aa
Hamza Rahman <hamza.rahman2016@outlook.com>
2021-09-19 17:49:59 +0500

    MVC Basics, Forms And Inputs

---
+++ WebAppDemo/Models/ValidateFever.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppDemo.Models
+{
+    public class ValidateFever
+    {
+        public static bool IsNotNull(float? value)
+        {
+            if (value.HasValue)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
